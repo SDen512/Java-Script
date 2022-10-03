83de643adf5ce13af474a6c87c2b7784e2de64c7
Denis Subach <moziden946@gmail.com>
2022-10-03 22:44:48 +0300

    Подготовка к написанию игры. Знакомство с promt, confirm, alert.

---
+++ JavaScript for Kids/play.js
@@ -0,0 +1,11 @@
+/* let someName = prompt("Как вас зовут?");
+console.log("Привет " +  someName); */
+
+/* let likesFootball = confirm("Тебе нравится футбол?");
+if(likesFootball) {
+   console.log("Ты молодец!");
+} else {
+   console.log("Приобщайся к спорту");
+} */
+
+alert("Это просто диалоговое окно для сообщений.");
