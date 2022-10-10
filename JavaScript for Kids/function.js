026505f257850613a046dc47d475d30849aea8ad
Denis Subach <moziden946@gmail.com>
2022-10-10 22:57:24 +0300

    Изучение функций в JS. Начало.

---
+++ JavaScript for Kids/function.js
@@ -0,0 +1,17 @@
+// Создание и вызов функции
+
+let myFirstFunction = function() {
+   console.log("Hello world!");
+}
+myFirstFunction();
+
+let sumNumber = function(a, b) {
+   console.log(a + b);
+}
+sumNumber(18, 489);
+
+let testFunction = function(test) {
+   return test * 5;
+}
+testFunction(87);
+console.log(testFunction(87));
