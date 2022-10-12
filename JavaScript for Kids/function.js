bf10074e599553d234503cab7dfeef2fba732717
Denis Subach <moziden946@gmail.com>
2022-10-12 22:20:08 +0300

    Практика с функциями.

--- JavaScript for Kids/function.js
+++ JavaScript for Kids/function.js
@@ -15,3 +15,54 @@ let testFunction = function(test) {
 }
 testFunction(87);
 console.log(testFunction(87));
+
+//Практика по функциям
+
+//Переписать, используя функцию
+
+/* let spanishTeams = ["Real", "Barselona", "Atletico", "Valensia"];
+let germanyTeams = ["Bayer", "Verder", "Boryssia", "Shalke"];
+let englishTeams = ["Chelsey", "Manchester City", "Arsenal", "Manchester United"];
+let hollandTeams = ["Ajaks", "PSV", "Feynord", "Alkmaar"];
+let randomSpanishTeam = spanishTeams[Math.floor(Math.random() * spanishTeams.length)];
+let randomEnglishTeam = englishTeams[Math.floor(Math.random() * englishTeams.length)];
+let randomGermanyTeam = germanyTeams[Math.floor(Math.random() * germanyTeams.length)];
+let randomHollandTeam = hollandTeams[Math.floor(Math.random() * hollandTeams.length)];
+let resultGrup = [randomSpanishTeam, randomEnglishTeam, randomGermanyTeam, randomHollandTeam].join(",");
+console.log(resultGrup); */
+
+/*  let randomElementArray = function(elementsArray) {
+   return elementsArray[Math.floor(Math.random() * elementsArray.length)];
+};
+let spanishTeams = ["Real", "Barselona", "Atletico", "Valensia"];
+let germanyTeams = ["Bayer", "Verder", "Boryssia", "Shalke"];
+let englishTeams = ["Chelsey", "Manchester City", "Arsenal", "Manchester United"];
+let hollandTeams = ["Ajaks", "PSV", "Feynord", "Alkmaar"];
+let resultGroup = [randomElementArray(spanishTeams), randomElementArray(germanyTeams), randomElementArray(englishTeams),randomElementArray(hollandTeams)].join(",");
+console.log(resultGroup); */
+ 
+let randomFootballGroup = function() {
+let randomElementArray = function(elementsArray) {
+   return elementsArray[Math.floor(Math.random() * elementsArray.length)];
+};
+let spanishTeams = ["Real", "Barselona", "Atletico", "Valensia"];
+let germanyTeams = ["Bayer", "Verder", "Boryssia", "Shalke"];
+let englishTeams = ["Chelsey", "Manchester City", "Arsenal", "Manchester United"];
+let hollandTeams = ["Ajaks", "PSV", "Feynord", "Alkmaar"];
+let resultGroup = [randomElementArray(spanishTeams), randomElementArray(germanyTeams), randomElementArray(englishTeams),randomElementArray(hollandTeams)].join(",");
+return resultGroup;
+}
+console.log(randomFootballGroup());
+
+//return вместо if...else
+
+let medalForScore = function (score) {
+   if(score < 3) {
+      return "Bronze";
+   }
+   if(score < 7) {
+      return "Silver";
+   }
+   return "Gold";
+};
+console.log(medalForScore(6));
