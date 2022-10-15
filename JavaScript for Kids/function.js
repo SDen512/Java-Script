f8b375fbb97b71006803cfab4a425cf77acf7d7a
Denis Subach <moziden946@gmail.com>
2022-10-15 18:16:04 +0300

    Переписывание игры, используя функции

--- JavaScript for Kids/function.js
+++ JavaScript for Kids/function.js
@@ -103,3 +103,108 @@ let areArraysSame = function(firstArray, secondArray) {
    return true;
 };
 console.log(areArraysSame(firstArray, secondArray));
+
+
+//Переписать игру "Виселица" с помощью функций
+
+//Функции для игры "Виселица"
+
+let pickWord = function() {
+   
+   // Возвращает случайно выбранное слово
+   
+   let words = [
+      "игра",
+      "школа",
+      "программа",
+      "макака",
+      "изучение",
+      "профессия",
+      "интернет",
+      "автомобиль",
+      "телефон"
+   ];
+   let word = words[Math.floor(Math.random() * words.length)];
+   return word;
+};
+
+let setupAnswerArray = function(word) {
+   
+   // Возвращает итоговый массив для заданного слова word
+
+   let answerArray = [];
+   for(let i = 0; i < word.length; i++) {
+   answerArray[i] = "_";
+}
+   return answerArray;
+};
+
+let showPlayerProgress = function(answerArray) {
+
+   // С помощью alert отображает текущее состояние игры
+
+   alert(answerArray.join(" "));
+
+};
+
+let getGuess = function() {
+
+   // Запрашивает ответ игрока с помощью prompt
+
+   let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.");
+   return guess;
+   
+};
+
+let updateGameState = function(guess, word, answerArray) {
+
+   // Обновляет answerArray согласно ответу игрока (guess)
+   // возвращает число, обозначающее, сколько раз буква guess
+ // встречается в слове, чтобы можно было обновить значение
+ // remainingLetters
+
+ for(let j = 0; j < word.length; j++) {
+   if(word[j] === guess && answerArray[j] === "_") {
+    return  answerArray[j] = guess;
+      //remainingLetters--;
+   } 
+}
+};
+
+let showAnswerAndCongratulatePlayer = function(answerArray) {
+
+   // С помощью alert показывает игроку отгаданное слово 
+ // и поздравляет его с победой
+
+   alert(answerArray.join(" "));
+   alert("Отлично! Было загадано слово " + word);
+
+};
+
+//Описание игры при помощи созданных функций
+
+// Загаданное слово
+let word = pickWord();
+
+//Итоговый массив
+let answerArray = setupAnswerArray(word);
+
+//сколько букв осталось угадать
+let remainingLetters = word.length;
+
+//игровой цикл
+while(remainingLetters > 0) {
+   showPlayerProgress(answerArray);
+   //Ответ игрока
+   let guess = getGuess();
+   if(guess === null) {
+      break;
+   } else if(guess.length !== 1) {
+      alert("Пожалуйста, введите только одну букву.");
+   }else {
+      //correctGuesses: количество открытых букв
+      let correctGuesses = updateGameState(guess, word, answerArray);
+      remainingLetters -=correctGuesses;
+   }
+}
+showAnswerAndCongratulatePlayer(answerArray);
