2ce5213e7af905fdbc23880ba2f4bceacb7d7403
Denis Subach <moziden946@gmail.com>
2022-10-18 22:35:22 +0300

    Создание новых элементов с помощью JQuery.

--- JavaScript for Kids/dom.js
+++ JavaScript for Kids/dom.js
@@ -5,5 +5,11 @@ elementHeading.innerHTML = newTextHeading; */
 
 //Замена заголовка с использованием JQuery
 
-let newTextHeading = prompt("Введите новый заголовок");
-$("#main-heading").text(newTextHeading);
+/* let newTextHeading = prompt("Введите новый заголовок");
+$("#main-heading").text(newTextHeading); */
+
+$(".test-section").append("<p>Это параграф, добавленный с помощью JQuery.</p>");
+
+ for(let i = 0; i < 3; i++) {
+   $("body").append("<ul><li>Это пункт списка, добавленный с помощью JQuery.</li></ul>");
+} 
